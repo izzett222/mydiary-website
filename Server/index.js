dac89a056b405515af4b681542e3560a23e718d7
izzett222 <51261911+izzett222@users.noreply.github.com>
2019-10-16 15:55:43 +0200

    Revert "#169177796 Finished the sign up endpoints"

--- Server/index.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-import app from './app';
-
-dotenv.config();
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`app running on port ${port}...`);
-});
